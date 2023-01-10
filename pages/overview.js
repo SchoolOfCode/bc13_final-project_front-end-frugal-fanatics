f8422696761320135837ae7f2d9a9443b35c63d1
shanedownes <shanedownes01@gmail.com>
2023-01-10 16:03:18 +0000

    Add Chart.JS donut chart test

--- pages/overview.js
+++ pages/overview.js
@@ -4,6 +4,10 @@ import {
 	secondaryNavigation,
 	setCurrentPage
 } from "../utils/navigation";
+//CHART.JS
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 setCurrentPage(primaryNavigation, "Overview");
 
@@ -15,6 +19,41 @@ const Overview = () => {
 			title="Overview Page"
 		>
 			<h1 className="text-9xl">HelloWorld</h1>
+			<div className="h-96 w-96"></div>
+			<div className="h-96 w-96">
+				{/* CHART.JS TEST */}
+				<Doughnut
+					data={{
+						labels: [
+							"Rent or Mortgage",
+							"Food and Groceries",
+							"Home Energy Bills",
+							"Council Tax",
+							"Credit Repayments",
+							"Leftover Income"
+						],
+						datasets: [
+							{
+								label: "My First Dataset",
+								data: [60, 50, 100, 80, 50, 100],
+								backgroundColor: [
+									"#B884FF",
+									"#FFD482",
+									"#F68DFF",
+									"#ABE6FF",
+									"#FFA59E",
+									"#F5F5F5"
+								],
+								hoverOffset: 4,
+								cutout: 140,
+								responsive: true,
+								maintainAspectRatio: true,
+								borderWidth: 0
+							}
+						]
+					}}
+				/>
+			</div>
 		</DashboardLayout>
 	);
 };
