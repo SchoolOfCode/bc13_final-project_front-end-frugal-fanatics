6b2c7c5e8f9bb2856c0460504e3ed91a51085c30
skdejong <stephaniekdejong@gmail.com>
2023-01-10 12:26:44 +0000

    Added DashboardLayout.js
    Co-authored-by: Callum <84125856+callum-cheshire@users.noreply.github.com>

---
+++ components/LandingLayout.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const LandingLayout = () => {
+	return (
+    <>
+        <Head>
+            <title>Form</title>
+            {/* <meta name="description" content={description} />
+            <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <main>
+			<h1>Hello world!</h1>
+		</main>
+    </>
+    );
+};
+
+export default LandingLayout;
