301d47aa03abb7420cbe883901f7b78a7a00bc75
shanedownes <shanedownes01@gmail.com>
2023-01-11 11:40:58 +0000

    Remove shapiro font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,7 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				shapiro: ["Shapiro, sans-serif"]
-			}
-		}
+		extend: {}
 	},
 	plugins: []
 };
