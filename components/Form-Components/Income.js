65018b4ce976cf82a0911f838328185c8943786e
Elliot Tasker <elliottasker@gmail.com>
2023-01-12 11:02:28 +0000

    up to the talk/lunch for the form, working on the income data storage

--- components/Form-Components/Income.js
+++ components/Form-Components/Income.js
@@ -6,14 +6,39 @@ import SubHeading from "../SubHeading";
 import FormInput from "../FormInput";
 import SubmitButton from "../SubmitButton";
 
-const Income = ({ formStep, nextFormStep }) => {
+const Income = ({ formStep, nextFormStep, formInput, setFormInput }) => {
+  const handleChange = (e) => {
+    setFormInput({ ...formInput, totalIncome: e.target.value });
+  };
+
   return (
     <>
-      <FormSteps formstep={formStep}/>
+      <FormSteps formstep={formStep} />
       <Heading title="What's your income after tax?" />
       <SubHeading subHeading="Gastropub hoodie vegan air plant kickstarter ascot adipisicing, hoodie twee small batch incididunt fit freegan meh." />
-      <FormInput labelname="Total Income (per month)" placeholder="0" />
-      <SubmitButton nextFormStep={nextFormStep} buttonText="Next Step ->"/>
+
+      {/* Need to create a Form that wraps the input and the button */}
+
+      <div className="flex w-4/5 flex-col pt-4">
+        <label className="pb-2">Total Income (per month) </label>
+        <input
+          handleChange={handleChange}
+          placeholder="0"
+          type="number"
+          onChange={handleChange}
+          className="h-12 w-full rounded-md bg-gray-200 p-4"
+          defaultValue="£"
+        ></input>
+      </div>
+
+      <div>
+        <button
+          className="text-1xl mt-12 w-max rounded-md border border-black p-2"
+          onClick={nextFormStep}
+        >
+          Next Step
+        </button>
+      </div>
     </>
   );
 };
--- components/FormInput.js
+++ components/FormInput.js
@@ -1,8 +1,8 @@
-const FormInput = ({ labelname, placeholder }) => {
+const FormInput = ({ labelname, placeholder, handleChange }) => {
     return (
         <div className="pt-4 flex flex-col w-4/5">
             <label className="pb-2">{labelname}</label>
-            <input /*type="number"*/ placeholder={placeholder} className="w-full h-12 rounded-md p-4 bg-gray-200" defaultValue="£"></input>
+            <input type="number" placeholder={placeholder} onChange={handleChange} className="w-full h-12 rounded-md p-4 bg-gray-200" defaultValue="£"></input>
         </div>
     );
 }
