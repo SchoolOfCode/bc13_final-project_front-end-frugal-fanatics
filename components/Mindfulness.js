5f05bf09846b66da2fc1f6c13e17db0c08732c97
Francesca Croutear-Foy <crofoys@gmail.com>
2023-01-12 13:58:41 +0000

    played around with midfullness component to sort out image rendering correctly

--- components/Mindfulness.js
+++ components/Mindfulness.js
@@ -1,17 +1,15 @@
 import React from "react";
 const Mindfulness = () => {
-  return (
-    <div className="stack flex w-full flex-col gap-10 rounded-2xl bg-white px-12 py-8 pb-14 max-[560px]:w-[60%] xl:w-[50%]">
-      <h2 className="text-202834  text-2xl text-[#202834]">Mindfulness</h2>
-      <div className="flex-col gap-6 min-[560px]:flex-row">
-        <div className="text-202834  text-1xl flex flex-col gap-6 text-[#202834] min-[560px]:flex-row">
-          "It's better to try and fail, then fail to try at all"
-          <div className="flex max-h-fit px-10">
-            <img src="Mindfulness_lady.png" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="stack relative flex w-full flex-col gap-10 rounded-2xl bg-white px-12 py-8 pb-14 max-[560px]:w-[60%] xl:w-[50%]">
+			<h2 className=" text-202834 text-2xl text-[#202834]">Mindfulness</h2>
+			<div className=" absolute top-1/2 right-1/2 flex flex-col gap-0 min-[560px]:flex-row">
+				"It's better to try and fail, then fail to try at all"
+				<div className="absolute right-1/4 left-1/2 flex h-full flex-col opacity-90">
+					<img src="Mindfulness_lady.png" />
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Mindfulness;
