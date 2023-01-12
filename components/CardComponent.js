71378bb38830f8b6ab300e263b268b2cd30329c2
mvhmz81 <mvhmz@yahoo.co.uk>
2023-01-12 19:07:14 +0000

    card placeholders wiuth hardcoded links set upt and working

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,14 +1,14 @@
 import React from "react";
+import Link from "next/Link";
 import { savingArticlesData } from "../data/savingsArtData";
+import next from "next";
 //button text has a default to read more or you can pass props
-const clickHandle = () => {
-  document.location.href = "https://stackoverflow.com/";
-};
 function CardComponent({
   imageSrc,
   title,
   description,
   buttonText = "Read More",
+  link,
 }) {
   return (
     <div class="flex justify-center">
@@ -19,13 +19,14 @@ function CardComponent({
         <div class="p-6">
           <h5 class="mb-2 text-xl font-medium text-gray-900">{title}</h5>
           <p class="mb-4 text-base text-gray-700">{description}</p>
-          <button
-            type="button"
-            class=" inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-            onClick={clickHandle}
-          >
-            {buttonText}
-          </button>
+          <a href={link} target="_blank">
+            <button
+              type="button"
+              class=" inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+            >
+              {buttonText}
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- pages/budgeting.js
+++ pages/budgeting.js
@@ -1,24 +1,36 @@
 import BudgetingSection from "../components/BudgetingSection";
+import CardComponent from "../components/CardComponent";
 import DashboardLayout from "../components/DashboardLayout";
+import { budgetArticlesData } from "../data/budgetArtData";
 import {
-	primaryNavigation,
-	secondaryNavigation,
-	setCurrentPage,
+  primaryNavigation,
+  secondaryNavigation,
+  setCurrentPage,
 } from "../utils/navigation";
 
 const Budgeting = ({ data }) => {
-	setCurrentPage(primaryNavigation, "Budgeting");
-	return (
-		<DashboardLayout
-			primaryNavigation={primaryNavigation}
-			secondaryNavigation={secondaryNavigation}
-			title="Budgeting Page"
-		>
-			<div className="flex flex-col items-center gap-8">
-				<BudgetingSection data={data} />
-			</div>
-		</DashboardLayout>
-	);
+  setCurrentPage(primaryNavigation, "Budgeting");
+  return (
+    <DashboardLayout
+      primaryNavigation={primaryNavigation}
+      secondaryNavigation={secondaryNavigation}
+      title="Budgeting Page"
+    >
+      <div className="flex flex-col items-center gap-8">
+        <BudgetingSection data={data} />
+      </div>
+      <div className="flex">
+        {budgetArticlesData.map((articles) => (
+          <CardComponent
+            imageSrc={articles.imageSrc}
+            title={articles.title}
+            description={articles.description}
+            link={articles.link}
+          />
+        ))}
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Budgeting;
