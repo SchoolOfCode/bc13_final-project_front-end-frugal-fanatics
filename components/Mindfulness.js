d53559a6db43986e987fdfd0e238e4ee138848a8
Francesca Croutear-Foy <crofoys@gmail.com>
2023-01-13 10:53:14 +0000

    created savings bar component and deleted as edits made to total savings for animation of bar on load, edited mindfullness img to size better with screen
    Co-authored-by: Shane Downes <112191948+shane-downes@users.noreply.github.com>
    Co-authored-by: mukti zavery <110194915+mvhmz81@users.noreply.github.com>

--- components/Mindfulness.js
+++ components/Mindfulness.js
@@ -1,11 +1,14 @@
 import React from "react";
+
 const Mindfulness = () => {
 	return (
-		<div className="stack relative flex w-full flex-col gap-10 rounded-2xl bg-white px-12 py-8 pb-14 max-[560px]:w-[60%] xl:w-[50%]">
-			<h2 className=" text-202834 text-2xl text-[#202834]">Mindfulness</h2>
-			<div className=" absolute top-1/2 right-1/2 flex flex-col gap-0 min-[560px]:flex-row">
-				"It's better to try and fail, then fail to try at all"
-				<div className="absolute right-1/4 left-1/2 flex h-full flex-col opacity-90">
+		<div className="stack flex w-full flex-col gap-1 rounded-2xl bg-white px-12 py-9 pb-4 max-[560px]:items-center xl:w-[50%]">
+			<h2 className="text-2xl font-medium text-[#202834]">Mindfulness</h2>
+			<div className="relative flex flex-col justify-center gap-5 min-[560px]:flex-row">
+				<div className="flex h-full w-full flex-col pt-10">
+					"It's better to try and fail, than fail to try at all"
+				</div>
+				<div className="flex pb-2 opacity-95">
 					<img src="Mindfulness_lady.png" />
 				</div>
 			</div>
