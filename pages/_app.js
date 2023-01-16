f7cfb3679eb24d8f68deead8ce2e58e084825486
Callum <84125856+callum-cheshire@users.noreply.github.com>
2023-01-16 16:01:04 +0000

    Created potential new data shapes which can be sent through post requests that match what the back end is expecting
    Co-authored-by: Stephanie de Jong <107569008+skdejong@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,31 @@ export default function App({ Component, pageProps }) {
 		savings: { total: 12900, goal: 20000 },
 	});
 
-	return <Component {...pageProps} data={data} setData={setData} />;
+	// potential data shape to be passed to back end and changes by data inputted by user into form:
+	const [incomeData, setIncomeData] = useState({
+		total_income: 0,
+		user_id: ""
+	})
+	const [expensesData, setExpensesData] = useState({
+            expense_type: "",
+            expense_amount: 0,
+            frequency: "",
+            user_id: 0
+	})
+	const [savingsData, setSavingsData] = useState({
+		overall_target: 5000,
+		savings_total: 100,
+		pot_totals: 100,
+		user_id: 1
+	})
+	const [userData, setUserData] = useState({
+		user_name: "",
+		auth_token: "",
+		first_name: "",
+		last_name: ""
+	})
+	
+	return <Component {...pageProps} data={data} setData={setData} incomeData={incomeData} setIncomeData={setIncomeData} expensesData={expensesData} setExpensesData={setExpensesData} savingsData={savingsData} setSavingsData={setSavingsData} userData={userData} setUserData={setUserData} />;
 }
 
 
