dbaa7a45b55a364415c1407577a73274e3a682f7
Francesca Croutear-Foy <crofoys@gmail.com>
2023-01-17 10:14:29 +0000

    added ignore eslint rules to next build for deployment
    Co-authored-by: Elliot <113646037+elliottasker@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true
-};
-
-module.exports = nextConfig;
+module.exports = {
+	eslint: {
+	  // Warning: This allows production builds to successfully complete even if
+	  // your project has ESLint errors.
+	  ignoreDuringBuilds: true,
+	},
+  }
