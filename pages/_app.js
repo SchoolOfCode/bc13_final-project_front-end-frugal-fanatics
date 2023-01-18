0e576120aa4df53debfc72c3825d1321b21a1bae
mvhmz81 <mvhmz@yahoo.co.uk>
2023-01-18 10:28:06 +0000

    imported superbase and added .env file data
    Co-authored-by: Shane Downes <112191948+shane-downes@users.noreply.github.com>
    Co-authored-by: Callum <84125856+callum-cheshire@users.noreply.github.com>Co-authored-by: Stephanie de Jong <107569008+skdejong@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,31 @@
 import "../styles/globals.css";
 import { useState } from "react";
-
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 export default function App({ Component, pageProps }) {
-	const [data, setData] = useState({
-		totalIncome: 3200,
-		expenses: [
-			{ label: "Rent or Mortgage", amount: 1200 },
-			{ label: "Food and Groceries", amount: 350 },
-			{ label: "Home Energy Bills", amount: 170 },
-			{ label: "Council Tax", amount: 240 },
-			{ label: "Credit Payments", amount: 500 },
-			{ label: "Leftover Income", amount: 740 },
-		],
-		savings: { total: 12900, goal: 20000 },
-	});
+  //unsure need tonresearch what this means later
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
-	return <Component {...pageProps} data={data} setData={setData} />;
+  // const [data, setData] = useState({
+  // 	totalIncome: 3200,
+  // 	expenses: [
+  // 		{ label: "Rent or Mortgage", amount: 1200 },
+  // 		{ label: "Food and Groceries", amount: 350 },
+  // 		{ label: "Home Energy Bills", amount: 170 },
+  // 		{ label: "Council Tax", amount: 240 },
+  // 		{ label: "Credit Payments", amount: 500 },
+  // 		{ label: "Leftover Income", amount: 740 },
+  // 	],
+  // 	savings: { total: 12900, goal: 20000 },
+  // });
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
-
-
