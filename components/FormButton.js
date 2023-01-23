b8218913aee97d300c27b5538daa908996472976
mvhmz81 <mvhmz@yahoo.co.uk>
2023-01-23 16:46:43 +0000

    modified added navbar and colourings
    Co-authored-by: Callum <84125856+callum-cheshire@users.noreply.github.com>

--- components/FormButton.js
+++ components/FormButton.js
@@ -2,19 +2,19 @@ import React from "react";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 
 const FormButton = ({ text }) => {
-	return (
-		<button
-			type="submit"
-			className="mt-16 inline-flex items-center justify-center rounded-lg border-2 border-slate-800 bg-[#108F68]
+  return (
+    <button
+      type="submit"
+      className="mt-16 inline-flex items-center justify-center rounded-lg border-2 bg-[#108F68]
 										px-7 py-2.5 text-lg font-normal tracking-wide text-white transition-colors hover:bg-[#10B981] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-		>
-			{text}
-			<ArrowRightIcon
-				className="ml-1.5 -mr-1 h-6 w-6 translate-y-px"
-				aria-hidden="true"
-			/>
-		</button>
-	);
+    >
+      {text}
+      <ArrowRightIcon
+        className="ml-1.5 -mr-1 h-6 w-6 translate-y-px"
+        aria-hidden="true"
+      />
+    </button>
+  );
 };
 
 export default FormButton;
