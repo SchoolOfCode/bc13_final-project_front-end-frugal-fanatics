ce00e71641e7b533353afefa63f9d6bab3f02bb6
skdejong <stephaniekdejong@gmail.com>
2023-01-24 12:32:11 +0000

    Add max and min width to FormContainer.js to make it responsive

--- components/FormContainer.js
+++ components/FormContainer.js
@@ -1,6 +1,6 @@
 const FormContainer = ({ children }) => {
 	return (
-		<section className="flex w-[540px] flex-col gap-6 px-6 pb-40 pt-16 md:px-0">
+		<section className="flex md:w-[540px] flex-col gap-6 px-6 pb-40 pt-16 md:px-0 max-w-[540px]">
 			{children}
 		</section>
 	);
