4126bcf422438b12c43151f36944ab995a9eec0a
shanedownes <shanedownes01@gmail.com>
2023-01-25 00:53:16 +0000

    Add piggy bank hero image to homepage and make it responsive

--- components/LandingDesktopNavbar.js
+++ components/LandingDesktopNavbar.js
@@ -3,14 +3,15 @@
 import React from "react";
 import Image from "next/image";
 
-const LandingNavbar = () => {
+const LandingDesktopNavbar = () => {
 	return (
-		<nav className="sticky top-0 z-50 hidden flex-wrap items-center justify-between border-b border-[#373C51] bg-[#1A1E2C] p-6 pl-8 pr-8 lg:flex">
+		<nav className="sticky top-0 z-50 hidden flex-wrap items-center justify-between border-b border-[#373C51] bg-[#1A1E2C] bg-opacity-70 p-6 pl-8 pr-8 bg-blur-xl md:flex">
 			<a href="/">
 				<Image src="logo.svg" alt="Logo" height="32" width="157" />
 			</a>
 		</nav>
 	);
+	z;
 };
 
-export default LandingNavbar;
+export default LandingDesktopNavbar;
